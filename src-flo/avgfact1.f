fcc93585fe2436e90687b2520223f4ede9c28a63
cpraveen <cpraveen@3eec099a-0224-0410-aa2c-71bfcdf8c47d>
2007-06-27 07:53:57 +0000

    Suppress ghost cell correction for solid boundary points. We just do inverse distance averaging since anyway the ghost-cell approach still does not give linear consistency.

--- src-flo/avgfact1.f
+++ src-flo/avgfact1.f
@@ -2,13 +2,13 @@ C----------------------------------------------------------------------------
 C Computes weights for vertex averaging using my new method
 C----------------------------------------------------------------------------
       subroutine avgfact(ptype, elem, edge, bdedge, esubp, spts, coord,
-     +                   tarea, afact)
+     +                   tarea, af)
       implicit none
       include 'param.h'
       integer          ptype(npmax), elem(3,ntmax), edge(2,nemax),
      +                 bdedge(2,nbpmax), esubp(mesubp,nbpmax), 
      +                 spts(nspmax)
-      double precision coord(2,npmax), tarea(ntmax), afact(3,npmax)
+      double precision coord(2,npmax), tarea(ntmax), af(3,npmax)
 
       integer          i, j, ip, it, e1, e2, p1, p2, v1, v2, v3
       double precision sax(npmax), say(npmax), sax2(npmax), say2(npmax),
@@ -22,9 +22,9 @@ C----------------------------------------------------------------------------
          sax2(i) = 0.0d0
          say2(i) = 0.0d0
          saxy(i) = 0.0d0
-         afact(1,i)= 0.0d0
-         afact(2,i)= 0.0d0
-         afact(3,i)= 0.0d0
+         af(1,i) = 0.0d0
+         af(2,i) = 0.0d0
+         af(3,i) = 0.0d0
       enddo
 
       do i=1,nt
@@ -40,35 +40,35 @@ C----------------------------------------------------------------------------
       enddo
 
 C Add ghost cell contributions for solid wall points
-      do i=1,nsp
-         ip = spts(i)
-         e1 = bdedge(1,i)
-         e2 = bdedge(2,i)
-         p1 = edge(1,e1)
-         p2 = edge(2,e2)
-         nx(i) = 0.0d0
-         ny(i) = 0.0d0
-         call bd_normal(coord(1,p1), coord(1,ip), coord(1,p2), 
-     +                  nx(i), ny(i))
-         do j=1,esubp(1,i)
-            it = esubp(j+1,i)
-            v1 = elem(1,it)
-            v2 = elem(2,it)
-            v3 = elem(3,it)
-            call afact2(coord(1,v1), coord(1,v2), coord(1,v3),
-     +                  coord(1,ip), nx(i), ny(i), sax(ip), say(ip), 
-     +                  sax2(ip), say2(ip), saxy(ip))
-         enddo
-
-      enddo
+c     do i=1,nsp
+c        ip = spts(i)
+c        e1 = bdedge(1,i)
+c        e2 = bdedge(2,i)
+c        p1 = edge(1,e1)
+c        p2 = edge(2,e2)
+c        nx(i) = 0.0d0
+c        ny(i) = 0.0d0
+c        call bd_normal(coord(1,p1), coord(1,ip), coord(1,p2), 
+c    +                  nx(i), ny(i))
+c        do j=1,esubp(1,i)
+c           it = esubp(j+1,i)
+c           v1 = elem(1,it)
+c           v2 = elem(2,it)
+c           v3 = elem(3,it)
+c           call afact2(coord(1,v1), coord(1,v2), coord(1,v3),
+c    +                  coord(1,ip), nx(i), ny(i), sax(ip), say(ip), 
+c    +                  sax2(ip), say2(ip), saxy(ip))
+c        enddo
+c     enddo
 
 C Compute weights by inverting least squares matrix
       do i=1,np
          call afact3(sax(i), say(i), sax2(i), say2(i), saxy(i),
-     +               afact(1,i))
-         if(ptype(i) .ne. interior .and. ptype(i) .ne. solid)then
-            afact(1,i)= 0.0d0
-            afact(2,i)= 0.0d0
+     +               af(1,i))
+c        if(ptype(i) .ne. interior .and. ptype(i) .ne. solid)then
+         if(ptype(i) .ne. interior)then
+            af(1,i)= 0.0d0
+            af(2,i)= 0.0d0
          endif
       enddo
 
@@ -78,12 +78,12 @@ C Compute denominator in vertex averaging formula
          v2 = elem(2,i)
          v3 = elem(3,i)
          call afact4(coord(1,v1), coord(1,v2), coord(1,v3),
-     +               tarea(i), afact(1,v1), afact(1,v2), 
-     +               afact(1,v3))
+     +               tarea(i), af(1,v1), af(1,v2), 
+     +               af(1,v3))
       enddo
 
 C Check min and max range of weights
-      call checkweights(elem, coord, afact)
+      call checkweights(elem, coord, af)
 
       return
       end
