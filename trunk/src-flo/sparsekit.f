0e89dec046ee4776ae4ba4b0a939e47c02432fbd
cpraveen <cpraveen@3eec099a-0224-0410-aa2c-71bfcdf8c47d>
2010-03-30 05:08:45 +0000

    Added lutsol subroutine from sparsekit

--- trunk/src-flo/sparsekit.f
+++ trunk/src-flo/sparsekit.f
@@ -292,6 +292,59 @@ c
 c
       return
       end
+c######################################################################
+      subroutine lutsol(n, y, x, alu, jlu, ju) 
+c######################################################################
+      real*8 x(n), y(n), alu(*)
+      integer n, jlu(*), ju(*)
+c-----------------------------------------------------------------------
+c
+c This routine solves the system  Transp(LU) x = y,
+c given an LU decomposition of a matrix stored in (alu, jlu, ju) 
+c modified sparse row format. Transp(M) is the transpose of M. 
+c----------------------------------------------------------------------- 
+c on entry:
+c n   = dimension of system 
+c y   = the right-hand-side vector
+c alu, jlu, ju 
+c     = the LU matrix as provided from the ILU routines. 
+c
+c on return
+c x   = solution of transp(LU) x = y.   
+c-----------------------------------------------------------------------
+c
+c Note: routine is in place: call lutsol (n, x, x, alu, jlu, ju) 
+c       will solve the system with rhs x and overwrite the result on x . 
+c 
+c-----------------------------------------------------------------------
+c local variables
+c
+        integer i,k
+c
+        do 10 i = 1, n
+           x(i) = y(i)
+ 10     continue
+c
+c forward solve (with U^T)
+c
+        do 20 i = 1, n
+           x(i) = x(i) * alu(i)
+           do 30 k=ju(i),jlu(i+1)-1
+              x(jlu(k)) = x(jlu(k)) - alu(k)* x(i)
+ 30        continue
+ 20     continue
+c     
+c     backward solve (with L^T)
+c     
+      do 40 i = n, 1, -1 
+         do 50 k=jlu(i),ju(i)-1
+              x(jlu(k)) = x(jlu(k)) - alu(k)*x(i)
+ 50      continue
+ 40   continue
+c
+      return
+      end
+c----------------end of lutsol -----------------------------------------
 c-----------------------------------------------------------------------
       subroutine iluk(n,a,ja,ia,lfil,alu,jlu,ju,levs,iwk,w,jw,ierr)
       implicit none 
